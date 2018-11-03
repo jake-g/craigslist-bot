f9bd0d875593e4b8447a0a34b36dcce5b5c30b5b
Jake Garrison <omonoid@uw.edu>
2018-11-03 16:54:38 -0700

    makeing non docker deploy

--- make_service.sh
+++ make_service.sh
@@ -1,13 +1,21 @@
 
-name="music-gear"
+# first arg should be service name 
+# and have a corresponding settings-[service-name].py
+name=$1
+if [ $# -eq 0 ]
+  then
+    echo "Error pass in service name"
+fi
 settings="settings-""$name"".py"
 echo "Making service: ""$name"
-DIR="deployment/services/craigslist-""$name""/"
+
+NAME="craigslist-""$name"
+DIR="deployment/services/""$NAME"/
 mkdir -p "$DIR"
 echo "Service folder: ""$DIR"
 
+# copy src files
 cp "$settings" "$DIR""settings.py"
-
 declare -a copy=("util.py" "scraper.py" "requirements.txt" "main_loop.py")
 for i in "${copy[@]}"
 do
@@ -23,20 +31,20 @@ Description=Run craigslist bot
 User=root
 Group=dietpi
 Type=simple
-ExecStart=/usr/bin/python3 /opt/"$DIR"main.py
-WorkingDirectory=/opt/"$DIR"
+ExecStart=/usr/bin/python3 /opt/"$NAME"/main.py
+WorkingDirectory=/opt/"$NAME"/
 Environment=SLACK_TOKEN=xoxp-139047967344-139832954196-153246389329-865763cff4c1680419bcfd1a9005dac1
 Restart=always
 RestartSec=2
 
 [Install]
-WantedBy=multi-user.target" > "$DIR""craigslist-""$name"".service"
+WantedBy=multi-user.target" > "$DIR""$NAME"".service"
 
 # install script steps
 echo "sudo apt-get install -y python3" > "$DIR""setup.sh"
 echo "pip install -r requirements.txt" >> "$DIR""setup.sh"
-echo "mkdir /opt/"$DIR"" >> "$DIR""setup.sh"
-echo "cp -r * /opt/"$DIR"" >> "$DIR""setup.sh"
-echo "cp "craigslist-""$name"".service" /lib/systemd/system/" >> "$DIR""setup.sh"
+echo "mkdir /opt/"$NAME"" >> "$DIR""setup.sh"
+echo "cp -r * /opt/"$NAME"" >> "$DIR""setup.sh"
+echo "cp "$NAME"".service" /lib/systemd/system/" >> "$DIR""setup.sh"
 
 
