3ad37df88dadfcb1a658e8babe47e3f9d53948a9
Jake Garrison <omonoid@uw.edu>
2018-11-03 16:43:09 -0700

    makeing non docker deploy

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-
+docker build -t craigslist . 
 docker run --name craig-bot-music --restart always -d -e SLACK_TOKEN=xoxp-139047967344-139832954196-153246389329-865763cff4c1680419bcfd1a9005dac1 craigslist
-docker exec -it craigslist-bot /bin/bash
+docker exec -it craig-bot /bin/bash
